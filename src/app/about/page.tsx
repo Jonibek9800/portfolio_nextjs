a333c77e71bb83165b7a163868a53a26610f9d8c
Jonibek M. Mahmudov <MahmudovJ@HUMO.local>
2024-06-18 17:49:22 +0500

    edit project page and about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,17 @@
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div>
+      Родился и вырос в г.Душанбе, окончил Технологический университет
+      Таджикистана, освоил программирование и преподавал информатику в средней
+      школе, а также проходил курс Junior Fronted Developer с 01.01.22 по
+      01.08.22, в школе IT-result Владилена Минина. Мои сильные
+      стороны-стрессоустойчивость, быстрая обучаемость, адаптация к любой среде.
+      Ключевые навыки Figma, HTML5, CSS3, SCSS, JavaScript, Rest API, React,
+      Next JS, Redux, Redux/Toolkit, Zustand, Git, Node.js, Type Script,
+      Bootstrap, Material ui, Кросс браузерная верстка. Начало кареры Humo Lab с
+      01.05.24 по настоящее время
+    </div>
+  );
 };
 
 export default About;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, League_Spartan } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/widgets/header";
+import { Footer } from "@/widgets/footer";
 
 const spartan = League_Spartan({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={spartan.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
